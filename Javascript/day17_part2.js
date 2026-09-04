9b895497816390222d3b225d21fddde3a940a19f
Vijaya Raja K M <vijayaraja.eng@gmail.com>
2026-09-04 16:25:34 +0530

    day17-assignment

---
+++ Javascript/day17_part2.js
@@ -0,0 +1,13 @@
+let a = 10;
+const b = 111;
+let name = "vijay";
+let age = 21;
+
+console.log(a);
+console.log("value is " + b);
+console.log("value is " + age);
+
+let peru = prompt("Enter your name");
+let ag1 = prompt("Enter your age");
+
+document.writeln(`Hi ${peru}, my age is ${ag1}`);
